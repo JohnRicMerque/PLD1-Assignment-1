9c6a14aa959d1bec581fe8e14e5b820ac55cd6ce
John Ric Merque <92630490+JohnRicMerque@users.noreply.github.com>
2021-10-24 10:00:21 +0800

    Delete Assignment 2 directory

--- Assignment 2/Program1.py
+++
@@ -1,7 +0,0 @@
-# This program asks the user for name, age and address and displays it
-
-name = input("What's your name?: ")
-age = input("How old are you?: ")
-address = input("Where do you live?: ")
-
-print(f"Hi, my name is {name}. I am {age} years old and I live in {address}.")
